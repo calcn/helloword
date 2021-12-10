a32ce1a2aefeed971512ee3107ae66b07780246e
Calendar <wangshp.smile@gmail.com>
2021-12-11 04:26:07 +0800

    add java file

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 10;
+        String name = "Calendar";
+        System.out.println("Hello World. I am " + name + ", and I am " + age + " years old.");
+    }
+}
